4beb538e07a413f35926be14e85b5399cf888874
EduSerodio <dudu9090@outlook.com.br>
2022-08-17 17:23:42 -0300

    Mesa-07

---
+++ Aula07/js/main.js
@@ -0,0 +1,7 @@
+
+import { toggleTheme } from './toggleTheme.js';
+import './felinesCard.js';
+import './updateCardImg.js'
+
+const toggle = document.querySelector('#switch');
+toggle.addEventListener('click', toggleTheme);
