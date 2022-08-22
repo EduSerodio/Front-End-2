71fb51a515c7c685d87702435386e1d6edd6d444
EduSerodio <dudu9090@outlook.com.br>
2022-08-22 20:00:47 -0300

    Modificação dos resultados finais

--- Aula02-Projeto/index.js
+++ Aula02-Projeto/index.js
@@ -1,18 +1,16 @@
 let pontoUsuario = 0
 let pontoMaquina = 0
-let contador = 0
+
 
 function validaEscolhaUsuario (btnValue, btnMaquina, pResult) {
 
     let escolhaMaquina = escolhaComputador(4, 1);
 
-
     function escolhaComputador(max, min) {
-   
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 
      if (escolhaMaquina == 2){
         document.getElementById(btnMaquina).innerHTML = '<img src="./img/img-rock.png" alt="img"></img>'
@@ -35,41 +33,20 @@ function validaEscolhaUsuario (btnValue, btnMaquina, pResult) {
     }
 
     if(pontuacao() == 1){
-      
         document.getElementById(pResult).innerHTML = 'Parabéns! Você venceu!'
-        document.getElementById('p1').innerHTML += " ganhou"
         console.log('entrou no if');
         pontoUsuario++
-        console.log(pontoUsuario);
     }else {
         document.getElementById(pResult).innerHTML = 'Você perdeu!'
-        document.getElementById('p1').innerHTML += " perdeu"
         console.log('entrou no else');
+        pontoMaquina++
     }
 
+    if (pontoUsuario ==2) {
+        document.querySelector('.ponto-usuario').textContent = pontoUsuario
+
+    }else if (pontoMaquina == 2) {
+        document.querySelector('.ponto-maquina').textContent = pontoMaquina
+    }
 
 }
-
-document.querySelector('.ponto-usuario').textContent = pontoUsuario
-
-/* const primeira = rodadas(escolhaUsuario1)
-const segunda = rodadas(escolhaUsuario2)
-const terceira = rodadas(escolhaUsuario3)
-
-if ((primeira + segunda + terceira)  >= 2 ) {
-    console.log("Você ganhou a porra do jogo")
-}else {
-    console.log("Você perdeu a porra do jogo")
-}
-
-console.log(primeira);
-console.log(segunda);
-console.log(terceira);  
-
-
-// --------------------------------Escolha do usuario----------------------------------------
-
-const btnPrimeiro = document.getElementById('bttn')
-
-console.log(btnPrimeiro.value);
- */
